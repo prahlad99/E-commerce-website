0ffb2e84fb4e4965ba914d362f05253baee635d1
prahlad99 <63383471+prahlad99@users.noreply.github.com>
2021-06-27 18:16:15 -0700

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Product,Contact,Orders,OrderUpdate
-admin.site.register(Product)
-admin.site.register(Orders)
-admin.site.register(Contact)
-admin.site.register(OrderUpdate)
-# Register your models here.
